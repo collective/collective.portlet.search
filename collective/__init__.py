63846f6083a10ee05ff1816599dca5b76650fd5a
Erico Andrei <erico@simplesconsultoria.com.br>
2011-06-20 17:11:53 -0300

    Update profile name

--- collective/__init__.py
+++ collective/__init__.py
@@ -5,11 +5,3 @@ except ImportError:
     from pkgutil import extend_path
     __path__ = extend_path(__path__, __name__)
 
-try:
-    from zopeskel.localcommands.plone import *
-    from zopeskel.localcommands.archetype import *
-
-    ArchetypeSubTemplate.parent_templates.append('portal_package')
-    PloneSubTemplate.parent_templates.append('portal_package')
-except ImportError:
-    pass
