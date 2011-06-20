00f2ee5302b4fea451cdc60adce9bba836e51749
Erico Andrei <erico@simplesconsultoria.com.br>
2011-06-20 18:24:01 -0300

    Splitting calendar logic from the rest of the page template

--- collective/portlet/search/portlets/advancedsearch.py
+++ collective/portlet/search/portlets/advancedsearch.py
@@ -1,5 +1,7 @@
+from StringIO import StringIO
 from zope.interface import Interface
 from zope.interface import implements
+from Acquisition import aq_inner
 
 from zope.component import getMultiAdapter
 
@@ -9,18 +11,62 @@ from plone.portlets.interfaces import IPortletDataProvider
 from zope import schema
 from zope.formlib import form
 from plone.app.vocabularies.catalog import SearchableTextSourceBinder
+from plone.memoize import ram
+from plone.memoize.compress import xhtml_compress
+
 
 from Products.ATContentTypes.interface import IATTopic
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from collective.portlet.search import util
-
+from plone.app.portlets import cache
 from plone.app.portlets.portlets.calendar import Renderer as BaseRenderer
 from plone.app.form.widgets.uberselectionwidget import UberSelectionWidget
 
 from collective.portlet.search import MessageFactory as _
 
 from zope.i18nmessageid import MessageFactory
+
+def _render_calendar_cachekey(fun, self):
+    # Create a cachekey for calendar template
+    context = aq_inner(self.context)
+    if not self.updated:
+        self.update()
+    
+    if self.calendar.getUseSession():
+        raise ram.DontCache()
+    else:
+        key = StringIO()
+        print >> key, 'AdvancedSearch'
+        print >> key, self.navigation_root_url
+        print >> key, cache.get_language(context, self.request)
+        print >> key, self.calendar.getFirstWeekDay()
+        year, month = self.getYearAndMonthToDisplay()
+        print >> key, year
+        print >> key, month
+                
+        return key.getvalue()
+
+def _render_portlet_cachekey(fun, self):
+    # Create a cachekey for calendar template
+    context = aq_inner(self.context)
+    if not self.updated:
+        self.update()
+    key = StringIO()
+    year, month = self.getYearAndMonthToDisplay()
+    print >> key, 'AdvancedSearch'
+    print >> key, self.title
+    print >> key, self.navigation_root_url
+    print >> key, cache.get_language(context, self.request)
+    print >> key, self.root
+    print >> key, self.use_calendar
+    print >> key, self.use_daterange
+    print >> key, self.use_portal_types
+    print >> key, self.use_keyword    
+    print >> key, year
+    print >> key, month
+            
+    return key.getvalue()
 
 class IAdvancedSearch(IPortletDataProvider):
     """
@@ -92,7 +138,8 @@ class Renderer(BaseRenderer):
     """
     
     _template = ViewPageTemplateFile('advancedsearch.pt')
-    
+    _templatecalendar = ViewPageTemplateFile('calendar.pt')
+        
     def __init__(self, context, request, view, manager, data):
         BaseRenderer.__init__(self, context, request, view, manager, data)
         portal_state = getMultiAdapter((context, request), name=u'plone_portal_state')
@@ -100,6 +147,10 @@ class Renderer(BaseRenderer):
         self.navigation_root_url = portal_state.navigation_root_url()
         self.portal_types = portal_tools.types()
         self.friendly_types = portal_state.friendly_types()
+    
+    @ram.cache(_render_portlet_cachekey)
+    def render(self):
+        return xhtml_compress(self._template())
     
     def search_form(self):
         return '%s/search_form' % self.navigation_root_url
@@ -147,6 +198,11 @@ class Renderer(BaseRenderer):
     @property
     def used_types(self):
         return self.friendly_types
+    
+    @ram.cache(_render_calendar_cachekey)
+    def renderedCalendar(self):
+        ''' Cache the calendar page template'''
+        return self._templatecalendar()
     
     def renderCalendar(self):
         """ recreates a sequence of weeks, by days each day is a mapping.
